dda853bc33d609a761a14b7baf19058bae5ef27c
instalator <vvvalt@mail.ru>
2016-04-23 23:42:58 +0700

    Summary

--- kodi.js
+++ kodi.js
@@ -28,10 +28,7 @@ adapter.on('objectChange', function (id, obj) {
 
 // is called if a subscribed state changes
 adapter.on('stateChange', function (id, state) {
-    // Warning, state can be null if it was deleted
-		 adapter.log.debug('stateChange ' + id + ' ' + JSON.stringify(state));   
-
-    // you can use the ack flag to detect if it is status (true) or command (false)
+		// adapter.log.debug('stateChange ' + id + ' ' + JSON.stringify(state));   
     if (state && !state.ack) {
 		var param = null;
 	    var ids = id.split(".");
@@ -56,7 +53,7 @@ adapter.on('stateChange', function (id, state) {
 		}
 		if (methods == 'Input_ExecuteAction'){
 			cmd = 'Input.ExecuteAction';
-			state.val = ids.toString();
+			//state.val = ids.toString();
 		}
 		
 			sendCommand(cmd,state,param);
@@ -134,21 +131,18 @@ function getConnection(cb) {
 }
 function GetPlayerId(_connection){
 	if (_connection) {
-		/* Get all active players and log them */
+		// Get all active players and log them 
 		_connection.Player.GetActivePlayers().then(function (players) {
 			adapter.log.debug('Active players:' + JSON.stringify(players));
-			/* Log the currently played item for all players */
+			// Log the currently played item for all players 
 			Promise.all(players.map(function(player) {
 				_connection.Player.GetItem(player.playerid).then(function(res) {
 					adapter.log.debug('Currently played for player[' + player.playerid + ']:' + JSON.stringify(res));
 					player_id = player.playerid;
-					//if (item.label){
 						adapter.setState('CurrentPlay', {val: res.item.label, ack: true});
-					//} else {
-					//	adapter.setState('CurrentPlay', {val: '', ack: true});
-					//}
 				});
 			}));
+			GetPlayProperties(_connection);
 		}, function (error) {
 			adapter.log.warn(error);
 			connection = null;
@@ -163,39 +157,59 @@ function GetPlayerId(_connection){
 	}
 	
 }
+function time(hour,min,sec){
+		var time = '';
+		hour = (parseInt(hour) < 10 ? '0': '') + hour;
+		min = (parseInt(min) < 10 ? '0': '') + min;
+		sec = (parseInt(sec) < 10 ? '0': '') + sec;
+		if (parseInt(hour) === 0){
+			time = min + '-' + sec;
+		} else {
+			time = hour + '-' + min + '-' + sec;
+		}
+		return time;
+}
+
 function GetPlayProperties(_connection){
 	_connection.run('Player.GetProperties', {"playerid":player_id,"properties":["audiostreams","canseek","currentaudiostream","currentsubtitle","partymode","playlistid","position","repeat","shuffled","speed","subtitleenabled","subtitles","time","totaltime","type"]}).then(function (res) {
 		//adapter.log.debug('Player.GetProperties:' + JSON.stringify(item));
-		adapter.setState('time', {val: res.time.hours+'-'+res.time.minutes+'-'+res.time.seconds, ack: true});
-		
+		adapter.setState('PlayingTime', {val: time(res.time.hours, res.time.minutes, res.time.seconds), ack: true});
+		adapter.setState('PlayingTotalTime', {val: time(res.totaltime.hours, res.totaltime.minutes, res.totaltime.seconds), ack: true});
+		adapter.setState('Repeat', {val: res.repeat, ack: true});
+		adapter.setState('Position', {val: res.position, ack: true});
+		adapter.setState('Playlistid', {val: res.playlistid, ack: true});
+		adapter.setState('Partymode', {val: res.partymode, ack: true});
 	});
-setTimeout(GetPlayProperties, 3000, _connection);
+//setTimeout(GetPlayProperties, 3000, _connection);
 }
-
+function GetProperties(_connection){
+		_connection.run('Application.GetProperties', {"properties":["volume","muted","name","version"]}).then(function (res) {
+			adapter.log.debug('Application.GetProperties:' + JSON.stringify(res));
+			adapter.setState('Mute', {val: res.muted, ack: true});
+			adapter.setState('Name', {val: res.name, ack: true});
+			adapter.setState('Volume', {val: res.volume, ack: true});
+			adapter.setState('Version', {val: res.version.major+'.'+res.version.minor+' '+res.version.tag, ack: true});
+			_connection.run('PVR.GetChannels', {"channelgroupid":"alltv","properties":["channel","channeltype","hidden","lastplayed","locked","thumbnail","broadcastnow"]}).then(function (item) {
+			adapter.setState('PVR.GetChannelsIPTV', {val: JSON.stringify(item), ack: true});
+			});
+			_connection.run('PVR.GetChannels', {"channelgroupid":"allradio","properties":["channel","channeltype","hidden","lastplayed","locked","thumbnail","broadcastnow"]}).then(function (item) {
+			adapter.setState('PVR.GetChannelsRadio', {val: JSON.stringify(item), ack: true});
+			});
+		}, function (error) {
+			adapter.log.warn(error);
+			connection = null;
+		}).catch(function (error) {
+			adapter.log.error(error);
+			connection = null;
+		});
+}
 function main() {
 
 	adapter.log.info('KODI connecting to: ' + adapter.config.ip + ':' + adapter.config.port);
 
 	getConnection(function (err, _connection) {
 		GetPlayerId(_connection);
-	
-			_connection.run('Application.GetProperties', {"properties":["volume","muted","name","version"]}).then(function (item) {
-				adapter.log.debug('Application.GetProperties:' + JSON.stringify(item));
-			//{"muted":false,"name":"Kodi","version":{"major":14,"minor":2,"revision":"20150326-7cc53a9","tag":"stable"},"volume":100}
-			//state.val = {'playerid': player_id};
-				//adapter.setState('CurrentPlay', {val: res.item.label, ack: true});
-				_connection.run('PVR.GetChannels', {"channelgroupid":"alltv","properties":["channel","channeltype","hidden","lastplayed","locked","thumbnail","broadcastnow"]}).then(function (item) {
-				adapter.log.debug('PVR.GetChannels:' + JSON.stringify(item));
-			//	{"jsonrpc":"2.0","id":1,"method":"PVR.GetChannels","params":{"channelgroupid":"alltv","properties":["channel","channeltype","hidden","lastplayed","locked","thumbnail","broadcastnow"]}}
-				});
-			}, function (error) {
-				adapter.log.warn(error);
-				connection = null;
-			}).catch(function (error) {
-				adapter.log.error(error);
-				connection = null;
-			});
-		
+		GetProperties(_connection);
 	});
 	
 	adapter.setObject('Player.YouTube', {
@@ -305,7 +319,16 @@ function main() {
         },
         native: {}
     });
-	var action = ['left','right','up','down','pageup','pagedown','select','highlight','parentdir','parentfolder','back','previousmenu','info','pause','stop','skipnext','skipprevious','fullscreen','aspectratio','stepforward','stepback','bigstepforward','bigstepback','osd','showsubtitles','nextsubtitle','codecinfo','nextpicture','previouspicture','zoomout','zoomin','playlist','queue','zoomnormal','zoomlevel1','zoomlevel2','zoomlevel3','zoomlevel4','zoomlevel5','zoomlevel6','zoomlevel7','zoomlevel8','zoomlevel9','nextcalibration','resetcalibration','analogmove','rotate','rotateccw','close','subtitledelayminus','subtitledelay','subtitledelayplus','audiodelayminus','audiodelay','audiodelayplus','subtitleshiftup','subtitleshiftdown','subtitlealign','audionextlanguage','verticalshiftup','verticalshiftdown','nextresolution','audiotoggledigital','number0','number1','number2','number3','number4','number5','number6','number7','number8','number9','osdleft','osdright','osdup','osddown','osdselect','osdvalueplus','osdvalueminus','smallstepback','fastforward','rewind','play','playpause','delete','copy','move','mplayerosd','hidesubmenu','screenshot','rename','togglewatched','scanitem','reloadkeymaps','volumeup','volumedown','mute','backspace','scrollup','scrolldown','analogfastforward','analogrewind','moveitemup','moveitemdown','contextmenu','shift','symbols','cursorleft','cursorright','showtime','analogseekforward','analogseekback','showpreset','presetlist','nextpreset','previouspreset','lockpreset','randompreset','increasevisrating','decreasevisrating','showvideomenu','enter','increaserating','decreaserating','togglefullscreen','nextscene','previousscene','nextletter','prevletter','jumpsms2','jumpsms3','jumpsms4','jumpsms5','jumpsms6','jumpsms7','jumpsms8','jumpsms9','filter','filterclear','filtersms2','filtersms3','filtersms4','filtersms5','filtersms6','filtersms7','filtersms8','filtersms9','firstpage','lastpage','guiprofile','red','green','yellow','blue','increasepar','decreasepar','volampup','volampdown','channelup','channeldown','previouschannelgroup','nextchannelgroup','leftclick','rightclick','middleclick','doubleclick','wheelup','wheeldown','mousedrag','mousemove','noop'];
+	adapter.setObject('Input_ExecuteAction', {
+        type: 'state',
+        common: {
+            name: 'Input_ExecuteAction',
+            type: 'string',
+            role: 'indicator'
+        },
+        native: {}
+    });
+	/*var action = ['left','right','up','down','pageup','pagedown','select','highlight','parentdir','parentfolder','back','previousmenu','info','pause','stop','skipnext','skipprevious','fullscreen','aspectratio','stepforward','stepback','bigstepforward','bigstepback','osd','showsubtitles','nextsubtitle','codecinfo','nextpicture','previouspicture','zoomout','zoomin','playlist','queue','zoomnormal','zoomlevel1','zoomlevel2','zoomlevel3','zoomlevel4','zoomlevel5','zoomlevel6','zoomlevel7','zoomlevel8','zoomlevel9','nextcalibration','resetcalibration','analogmove','rotate','rotateccw','close','subtitledelayminus','subtitledelay','subtitledelayplus','audiodelayminus','audiodelay','audiodelayplus','subtitleshiftup','subtitleshiftdown','subtitlealign','audionextlanguage','verticalshiftup','verticalshiftdown','nextresolution','audiotoggledigital','number0','number1','number2','number3','number4','number5','number6','number7','number8','number9','osdleft','osdright','osdup','osddown','osdselect','osdvalueplus','osdvalueminus','smallstepback','fastforward','rewind','play','playpause','delete','copy','move','mplayerosd','hidesubmenu','screenshot','rename','togglewatched','scanitem','reloadkeymaps','volumeup','volumedown','mute','backspace','scrollup','scrolldown','analogfastforward','analogrewind','moveitemup','moveitemdown','contextmenu','shift','symbols','cursorleft','cursorright','showtime','analogseekforward','analogseekback','showpreset','presetlist','nextpreset','previouspreset','lockpreset','randompreset','increasevisrating','decreasevisrating','showvideomenu','enter','increaserating','decreaserating','togglefullscreen','nextscene','previousscene','nextletter','prevletter','jumpsms2','jumpsms3','jumpsms4','jumpsms5','jumpsms6','jumpsms7','jumpsms8','jumpsms9','filter','filterclear','filtersms2','filtersms3','filtersms4','filtersms5','filtersms6','filtersms7','filtersms8','filtersms9','firstpage','lastpage','guiprofile','red','green','yellow','blue','increasepar','decreasepar','volampup','volampdown','channelup','channeldown','previouschannelgroup','nextchannelgroup','leftclick','rightclick','middleclick','doubleclick','wheelup','wheeldown','mousedrag','mousemove','noop'];
 	
 	action.forEach(function(item, i, arr) {
 		adapter.setObject('Input_ExecuteAction.'+item, {
@@ -318,7 +341,7 @@ function main() {
 			native: {}
 		});								
 	});
-		
+	*/	
 	
 	
     // in this template all states changes inside the adapters namespace are subscribed
